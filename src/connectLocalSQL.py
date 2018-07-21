3d6b14f69bb29297b2a9017a708883ffbdd086e3
kapipara <kapipara_pcmail@yahoo.co.jp>
2018-07-22 01:10:40 +0900

    [add]生SQLのPythonコード

--- src/connectLocalSQL.py
+++ src/connectLocalSQL.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
+import pymysql.cursors
+import datetime
+import time
+
+connection = pymysql.connect(host="localhost", user="possys_logic", password="pospos", db="kapi",charset="utf8m64")
+
+now = datetime.datetime.now()
+
+try:
+    with connection.cursors() as cursor:
+        sql = "insert into kapi"
