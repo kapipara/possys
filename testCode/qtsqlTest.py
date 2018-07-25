a9b14d46ffb04e2c26412fafa2003a044a4bcf95
kapipara <kapipara_pcmail@yahoo.co.jp>
2018-07-25 21:00:18 +0900

    kagisysを参考にちょっと書いてみた

--- testCode/qtsqlTest.py
+++ testCode/qtsqlTest.py
@@ -1,4 +1,13 @@
 #! /usr/bin/python
 #! -*- coding:utf-8 -*-
 
-import Configparser
+import ConfigParser
+import time
+from PyQt4 import QtSql
+
+class Database:
+    def __init__(self):
+        self.config = configparser.SafeConfigParser()
+        self.config.read('POSSYS_PATH')
+        self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
+        self.db.setHostName
