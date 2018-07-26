dd00c11d8b3e4e0b84b681eae95a231e56fe4bb8
kapipara <kapipara_pcmail@yahoo.co.jp>
2018-07-26 17:20:55 +0900

    今日の進捗(database接続部分とコンフィグファイル読み取り部分を作った)

--- testCode/qtsqlTest.py
+++ testCode/qtsqlTest.py
@@ -1,13 +1,39 @@
 #! /usr/bin/python
 #! -*- coding:utf-8 -*-
+# Python3で動くよ！
 
-import ConfigParser
+# configファイル
+import configparser
+# 時間取得用
 import time
-from PyQt4 import QtSql
+# データベースアクセス
+import mysql.connector
 
+# データベース操作クラス
 class Database:
     def __init__(self):
+        # configファイルを参照
         self.config = configparser.SafeConfigParser()
-        self.config.read('/home/kapipi/possys/testCode/setting.ini')
-        self.db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
-        self.db.setHostName
+        self.config.read('setting.ini')
+
+        # データベースを参照
+        # 各値はconfigファイルのDATABASEセクションから取得
+        self.db = mysql.connector.connect(host     = config.get('DATABASE','hostname'),
+                                          user     = config.get('DATABASE','username'),
+                                          password = config.get('DATABASE','password'),
+                                          database = config.get('DATABASE','databaseName')
+                                         )
+        # データベース対話クラスのインスタンスを作成
+        cursor = db.cursor()
+
+
+    def __open(self):
+        # データベースが開けなかったときの回避処理
+        if not self.db.open():
+            print("[ERROR] Database can't open!\n")
+            exit()
+
+    def checkIDm(self, IDm):
+        self.__open()
+        query = QtSql.QSqlQuery()
+        query.prepare('SELECT * FROM NFCID WHERE IDm=')
